b2d5808a851cbda1bb0a1175cb2f551a2b91f159
SergyeyevVladimir <sergyeyev.vova@mail.ru>
2016-04-07 19:55:11 +0300

    +add RS005

--- src/Rastipuzzo.dpr
+++ src/Rastipuzzo.dpr
@@ -1,21 +1,20 @@
-program DaVinci;
+program Rastipuzzo;
 
 uses
   System.StartUpCopy,
   FMX.Forms,
-  FMX.Helpers.Controls in 'lib\FMX.Helpers.Controls.pas',
-  Network in 'lib\Network.pas',
-  Restourant.JsonDataObject in 'lib\Restourant.JsonDataObject.pas',
-  Restourant.JsonDatabase in 'lib\Restourant.JsonDatabase.pas',
-  Restourant.Forms.Main in 'forms\Restourant.Forms.Main.pas' {FormMain},
   Restourant.Consts.Colors in 'const\Restourant.Consts.Colors.pas',
   Restourant.Consts.Database in 'const\Restourant.Consts.Database.pas',
   Restourant.Consts.Filenames in 'const\Restourant.Consts.Filenames.pas',
   Restourant.Consts.Strings in 'const\Restourant.Consts.Strings.pas',
-  Restourant.Consts.UserData in 'const\Restourant.Consts.UserData.pas';
+  Restourant.Consts.UserData in 'const\Restourant.Consts.UserData.pas',
+  Restourant.Forms.Main in 'forms\Restourant.Forms.Main.pas',
+  FMX.Helpers.Controls in 'lib\FMX.Helpers.Controls.pas',
+  Network in 'lib\Network.pas',
+  Restourant.JsonDatabase in 'lib\Restourant.JsonDatabase.pas',
+  Restourant.JsonDataObject in 'lib\Restourant.JsonDataObject.pas';
 
 {$R *.res}
-{$R data\db.res}
 {$R ..\res\images.res}
 
 begin
